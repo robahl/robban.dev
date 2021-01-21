028d82ae7bec3d7544a82d14529acfef46e9ac15
Robert Ahlberg <rob.ahlberg@gmail.com>
2021-01-21 01:23:30 +0100

    Remove next image comp

--- pages/his-work.js
+++ pages/his-work.js
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import Layout from '../components/layout';
 import styles from '../styles/HisWork.module.css';
 
@@ -7,11 +6,10 @@ export default function () {
   return (
     <Layout title="his work">
       <a href="https://github.com/robahl">
-        <Image
+        <img
           className={styles.githubLogo}
           src="/github-logo.svg"
           alt="github logo"
-          layout="fill"
         />
       </a>
     </Layout>
