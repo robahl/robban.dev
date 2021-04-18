cfa17a73be9a4bfec4a505cb7ee7dbc0db8f7080
Robert Ahlberg <rob.ahlberg@gmail.com>
2021-04-18 22:21:56 +0200

    Revert "Add GTM"
    This reverts commit 0dba3449ec635debdec362730af0b16a5f3041ef.

--- pages/_document.js
+++ pages/_document.js
@@ -11,16 +11,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MFXM938');`,
-            }}
-          />
-
-          <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-67449721-5"
           />
@@ -35,13 +25,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-MFXM938"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"></iframe>
-          </noscript>
           <Main />
           <NextScript />
         </body>
