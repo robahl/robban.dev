4902cbd610115f577d2177dab6c2509d4d225bca
Robert Ahlberg <rob.ahlberg@gmail.com>
2022-10-14 17:15:06 +0200

    Update the-developer.js

--- pages/the-developer.js
+++ pages/the-developer.js
@@ -5,9 +5,9 @@ export default function TheDeveloper() {
   return (
     <Layout title="the developer">
       <p>
-        robert ahlberg. creating the greatest igaming platform money can buy at{' '}
-        <a href="https://codebet.com" target="__blank">
-          codebet
+        robert ahlberg. creating the greatest apps money can buy at{' '}
+        <a href="https://www.pixelpappa.com" target="__blank">
+          pixelpappa
         </a>
         .
       </p>
